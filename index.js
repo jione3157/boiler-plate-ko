d5fce24ed2e7abe740e0be013120322876c6a3e6
jione <kjw3157@naver.com>
2020-08-13 07:24:19 +0900

    로그인 안됨

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const app = express()
 const port = 3000
 
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const {User} = require("./models/User")
-
+const config = require('./config/key')
 
 app.use(bodyParser.urlencoded({extended: true})) // application/x-www-form-urlencoded
 app.use(bodyParser.json()) //application/json
+app.use(cookieParser())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jione:ja546580@jione.2qflm.mongodb.net/test?retryWrites=true&w=majority', {
+const { json } = require('body-parser')
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=>console.log('MogoDB Connected...'))
 .catch(err => console.log(err))
@@ -24,13 +27,45 @@ app.post('/register', (req, res) => {
     // 그것들을 데이터 베이스에 넣어준다.
 
     const user = new User(req.body) // 유저 정보가 requset.body에 들어있음
-    
     user.save((err, userInfo) => { // 정보들이 유저 모델에 저장
         if(err) return res.json({ success: false, err })//저장 실패했을 경우
         return res.status(200).json({
             success: true // 저장 성공했을 경우
         })
     })
+})
+
+app.post('/login',(req,res) => {
+
+    // 요청된 이메일이 데이터베이스에서 있는지 찾는다.
+    User.findOne({ email: req.body.email},(err, user) => {
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+        user.comparePassword(req.body.password, (err,isMatch) => {
+            if(!isMatch) // 비밀번호가 같지않음
+            return res.json({loginSuccess:false, message: "비밀번호가 틀렸습니다."})
+        
+            // 비밀번호까지 맞다면 토큰을 생성하기.
+        user.generateToken((err,user) => {
+            if(err) return res.status(400).send(err) // 에러가 있을 경우
+
+            // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id})
+
+             })
+             
+        })
+
+    })
+
 })
 
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10  // salt가 몇 글자 인지
+const jwt = require('jsonwebtoken')
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -31,6 +35,55 @@ const userSchema = mongoose.Schema({
     }
 
 })
+
+userSchema.pre('save',function( next ) {
+    var user = this // userSchema를 가리킴
+
+    if(user.isModified('password')){
+            // 비밀번호가 변경될때만 비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err) // 에러가 발생했을 때
+     
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if(err) return next(err) // 에러가 발생했을 때
+                user.password = hash // 암호화가 성공하면 password를 hash로 교체
+                next()
+            })
+        }) 
+
+    }
+    else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) { // 비밀번호 비교 메소드
+    // plainPassword 1234567 암호화된 비밀번호 $2b$10$HW5QnqP3SE6Ux85ezceHJeKrSphylPCYB587mmeGeTBgpkP3YWomy
+    // plainPassword를 암호화해 비교해야함 복호화할 수 없음
+    bcrypt.compare(plainPassword,this.password,function(err,isMatch){
+        if(err) return cb(err) // 비밀번호가 같지않음
+        cb(null,isMatch) // 비밀번호가 같음
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    
+    var user = this
+
+    // jsonwebtoken을 이용해서 token을 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // user._id + 'secretToken' = token
+    // ->
+    // 'secretToken' -> user._id
+
+    user.token = token
+    user.save(function(err,user){ // user DB에 토큰 저장 
+        if(err) return cb(err)
+        cb(null,user)
+    })
+    
+}
+
 
 const User = mongoose.model('User', userSchema)
 
